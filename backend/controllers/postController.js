94295238c79a91910ef38216e7855539998e6e48
Sneha Sharma <snehasharmaa912@gmail.com>
2026-07-08 20:46:28 +0530

    feat: protect post creation and add like toggle endpoint

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -15,7 +15,11 @@ export const getPostBySlug = async (req, res) => {
 
 export const createPost = async (req, res) => {
   try {
-    const post = await Post.create(req.body)
+    const post = await Post.create({
+      ...req.body,
+      author: req.user._id,
+      authorName: req.user.name,
+    })
     res.status(201).json(post)
   } catch (err) {
     res.status(400).json({ message: err.message })
@@ -23,16 +27,39 @@ export const createPost = async (req, res) => {
 }
 
 export const updatePost = async (req, res) => {
-  const post = await Post.findOneAndUpdate({ slug: req.params.slug }, req.body, {
-    new: true,
-    runValidators: true,
-  })
+  const post = await Post.findOne({ slug: req.params.slug })
   if (!post) return res.status(404).json({ message: 'Post not found' })
+  if (post.author.toString() !== req.user._id.toString()) {
+    return res.status(403).json({ message: 'Not authorized to edit this post' })
+  }
+  Object.assign(post, req.body)
+  await post.save()
   res.json(post)
 }
 
 export const deletePost = async (req, res) => {
-  const post = await Post.findOneAndDelete({ slug: req.params.slug })
+  const post = await Post.findOne({ slug: req.params.slug })
   if (!post) return res.status(404).json({ message: 'Post not found' })
+  if (post.author.toString() !== req.user._id.toString()) {
+    return res.status(403).json({ message: 'Not authorized to delete this post' })
+  }
+  await post.deleteOne()
   res.json({ message: 'Post deleted' })
+}
+
+export const toggleLike = async (req, res) => {
+  const post = await Post.findOne({ slug: req.params.slug })
+  if (!post) return res.status(404).json({ message: 'Post not found' })
+
+  const userId = req.user._id.toString()
+  const alreadyLiked = post.likes.some((id) => id.toString() === userId)
+
+  if (alreadyLiked) {
+    post.likes = post.likes.filter((id) => id.toString() !== userId)
+  } else {
+    post.likes.push(req.user._id)
+  }
+
+  await post.save()
+  res.json({ likes: post.likes.length, liked: !alreadyLiked })
 }
