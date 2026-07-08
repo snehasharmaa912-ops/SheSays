a62e0867e3906e3f0f830f9b10f3487a11079e3f
Sneha Sharma <snehasharmaa912@gmail.com>
2026-07-08 19:30:53 +0530

    feat: add contact route

---
+++ backend/routes/contactRoutes.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import { sendContact } from '../controllers/contactController.js'
+const router = express.Router()
+router.post('/', sendContact)
+
+export default router
